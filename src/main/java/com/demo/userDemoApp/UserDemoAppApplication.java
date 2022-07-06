6e890af456b1fe6da89e682ae7b66a4465a4f35f
vikashjha26 <jhavikash1990@gmail.com>
2022-07-06 22:58:11 +0530

    deleted old files

---
+++ src/main/java/com/demo/userDemoApp/UserDemoAppApplication.java
@@ -0,0 +1,15 @@
+package com.demo.userDemoApp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDemoAppApplication.class, args);
+		
+	}
+
+}
